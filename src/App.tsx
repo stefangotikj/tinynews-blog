6c0e083ee0801bccb48fcbc194d0a8d86de61801
Stefan Gotikj <hi@tinynews.site>
2025-07-30 00:47:02 +0200

    update app.tsx
    code smell.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { Suspense, lazy, useEffect } from "react";
 import NotFound from "./pages/NotFound";
 import { siteConfig } from "./site.config";
